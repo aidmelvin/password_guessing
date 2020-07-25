f723671b8f0f897957ab8f387f0a7a13389ad7a0
Aidan Melvin <aidmelvin.314@gmail.com>
2020-07-25 13:28:05 -0400

    finished the program

--- demo.c
+++ demo.c
@@ -4,12 +4,26 @@
 #include <stdlib.h> 
 #include <time.h> 
   
+
+void delay(int milli_seconds) 
+{ 
+    // Storing start time 
+    clock_t start_time = clock(); 
+  
+    // looping till required time is not achieved 
+    while (clock() < start_time + milli_seconds)
+    {
+
+    }
+} 
 // Generates and prints 'count' random 
 // numbers in range [lower, upper]. 
 int generateRandom(int lower, int upper)
 { 
-    return (rand() % (upper - lower + 1)) + lower; 
+    delay(500000);
+    return ((unsigned int)time(NULL) % (upper - lower + 1)) + lower;
 }
+
 
 int main () {
     printf("welcome to the password checker game \n");
@@ -17,11 +31,13 @@ int main () {
 
     int i = 0;
     int number[4];
-    printf("here is the actual number: \n");
+    // printf("here is the actual number: \n");
     for (i = 0; i < 4; i ++)
     {
-        number[i] = generateRandom(1, 10);
-        printf("%i", number[i]);
+        number[i] = generateRandom(1, 9);
+        printf("one number generated");
+        printf("\n");
+        // printf("%i", number[i]);
     }
     printf("\n");
 
@@ -30,15 +46,25 @@ int main () {
 
     for  (i = 0; i < 4; i ++)
     {
+        guess = -1;
         while (guess != number[i])
         {
             printf("You have successfully guessed ");
             printf("%i", i);
             printf(" digits  of the password. Enter your guess below: \n");
             scanf("%d",  &guess);
+            printf("\nGuess: ");
+            printf("%i", guess);
+            printf("\n");
         }
     }
 
+    printf("Congratulations! You successfully guessed the number. It was: \n");
+    for (i = 0; i < 4; i ++)
+    {
+        printf("%i", number[i]);
+    }
+    printf("\n");
     // printf("why is this not working\n");
     // // compare_strings("string 1", "string 2");
 
